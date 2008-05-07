d75115da73071fa6f4d96a614ee96c2e3fbd6ae5
Yves Lafon <ylafon@w3.org>
2008-05-07 16:44:33 +0000

    some doc

--- grib.h
+++ grib.h
@@ -1,5 +1,5 @@
 /**
- * $Id: grib.h,v 1.5 2008/05/07 11:20:22 ylafon Exp $
+ * $Id: grib.h,v 1.6 2008/05/07 16:44:33 ylafon Exp $
  *
  * (c) 2008 by Yves Lafon
  *      See COPYING file for copying and redistribution conditions.
@@ -24,14 +24,28 @@
 #include "defs.h"
 #include "types.h"
 
+/**
+ * init global wind structures in the context based on a grib file
+ * containing only UGRD/VGRD entries in matched pairs.
+ * time offest is set to compile time default. (GRIB_TIME_OFFSET)
+ * it corresponds by default to 35 to 40mn time delay, from VLM average.
+ * unit is seconds.
+ */
 void init_grib();
 
-/* init structures from the grib file, a time offset (in seconds) is applied
-   positive time offset means that the structure will be drifted in the 
-   future, so the boats will be late.
-*/
+/**
+ * init global wind structures in the context based on a grib file
+ * containing only UGRD/VGRD entries in matched pairs.
+ * time offest is given as a parameter (see above)
+ */
 void init_grib_offset PARAM1(long);
+/**
+ * set the grib offset (see above) without having to re-read the grib
+ */
 void set_grib_offset PARAM1(long);
+/**
+ * get the current grib offset (see above)
+ */
 long get_grib_offset ();
 
 #endif /* _GRIB_H_ */
