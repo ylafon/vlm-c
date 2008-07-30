549e9f522997f9743fc90ca37050d1fe4fba03ca
Yves Lafon <ylafon@w3.org>
2008-07-30 19:07:34 +0000

    resizing of the segment if needed when copying the wind tables to the segment

--- shmem.c
+++ shmem.c
@@ -1,5 +1,5 @@
 /**
- * $Id: shmem.c,v 1.7 2008/07/30 17:04:46 ylafon Exp $
+ * $Id: shmem.c,v 1.8 2008/07/30 19:07:34 ylafon Exp $
  *
  * (c) 2008 by Yves Lafon
  *      See COPYING file for copying and redistribution conditions.
@@ -125,7 +125,7 @@ int create_grib_shmid(winds_prev *windtable) {
            ((windtable) ? windtable->nb_prevs : DEFAULT_NB_SHARED_GRIB_ENTRIES);
   
   /* create the shared memory segment */
-  shmid = shmget(VLM_GRIB_MEM_KEY, needed_bytes, IPC_CREAT|0666);
+  shmid = shmget(VLM_GRIB_MEM_KEY, needed_bytes, IPC_CREAT|0644);
   if (shmid == -1) {
     /* failed */
     fprintf(stderr, "Unable to create shared memory segment VLMGRB\n");
@@ -142,7 +142,7 @@ int get_grib_shmid() {
   int shmid;
   /* FIXME do we need to read the size, then redo the shmget ? I guess not 
      but it is possible... */
-  shmid = shmget(VLM_GRIB_MEM_KEY, 0, 0666);
+  shmid = shmget(VLM_GRIB_MEM_KEY, 0, 0644);
   return shmid;
 }
 
@@ -160,22 +160,66 @@ void *get_grib_shmem(int shmid, int readonly) {
   }
   return addr;
 }
-
-void copy_grib_array_to_shmem(winds_prev *windtable, void *memseg) {
+/**
+ * Copy the wind previsions (in windtable) in the shared memory segment
+ * If the segment is too small, it may be resized 
+ * @param shmid an int, the id of the shared memory segment
+ * @param windtable a pointer to a <code>winds_prev</code> structure
+ * @param memseg a generic (void *) pointer representing the segment
+ * @return an int, the shmid. If it changed, the user must check that
+ * the memseg pointer is still valid
+ */
+int copy_grib_array_to_shmem(int shmid, winds_prev *windtable, void *memseg) {
   long nb_bytes;
-  int *intarray, i, nb_prevs;
+  int *intarray, i, nb_prevs, ok, maxprev;
   time_t *tarray;
   long used_bytes;
   winds *windarray;
+  struct shmid_ds shminfo;
+  
+  if (shmctl(shmid, IPC_STAT , &shminfo) == -1) {
+    fprintf(stderr, "Unable to access information on GRIB segment\n");
+    return -1;
+  }
 
-  /* FIXME, ensure that the size of the segment is enough to hold the complete
-     structure (using shmctl to get the size) */
+  maxprev = 0;
   nb_prevs = windtable->nb_prevs;
   /* be sure to lock the semaphore before using this function */
   nb_bytes = nb_prevs * sizeof(winds);
   printf("Got %d entries in windtable\n", windtable->nb_prevs);
   printf("Bytes used: %ld (winds struct size is %ld)\n",nb_bytes,sizeof(winds));
+  ok = (shminfo.shm_segsz > nb_bytes);
+  printf("Segment size: %ld %s\n",  (long)shminfo.shm_segsz, 
+	 (ok) ? "OK" : "NOT OK");
 
+  if (!ok) {
+    /* we need to reallocate... here, we should own the semaphore, so it is 
+       safe to destroy everything and restart */
+    shmdt(memseg);
+    if (shmctl(shmid, IPC_RMID, &shminfo) == -1) {
+      fprintf(stderr, "Unable to resize GRIB segment\n");
+      maxprev = shminfo.shm_segsz / sizeof(winds);
+    } else {
+      shmid = create_grib_shmid(windtable);
+      if (shmid == -1) {
+	/* ok we are in trouble, we destroyed the previous existing segment
+	   now we can't create a bigger new one */
+	shmid = shmget(VLM_GRIB_MEM_KEY, shminfo.shm_segsz, IPC_CREAT|0644);
+	if (shmid == -1) {
+	  /* we are in deeeeeeep trouble, abort */
+	  return -1;
+	}
+	maxprev = shminfo.shm_segsz / sizeof(winds);
+	return shmid;
+      }
+      /* don't forget to reattach the new memory */
+      memseg = shmat(shmid, memseg, 0);
+    }
+  }
+  /* if we are in danger of an overflow... limit the amount copied */
+  if (maxprev && nb_prevs > maxprev) {
+    nb_prevs = maxprev;
+  }
   intarray = (int *) memseg;
   *intarray =  nb_prevs;
   used_bytes = sizeof(int);
@@ -193,6 +237,7 @@ void copy_grib_array_to_shmem(winds_prev *windtable, void *memseg) {
   for (i=0; i<nb_prevs; i++) {
     memcpy(windarray++, windtable->wind[i], sizeof(winds));
   }
+  return shmid;
 }
   
 void construct_grib_array_from_shmem(winds_prev *windtable, void *memseg) {
@@ -227,7 +272,41 @@ void construct_grib_array_from_shmem(winds_prev *windtable, void *memseg) {
   }
   windtable->nb_prevs = nb_prevs;
 }
-  
+ 
+void allocate_grib_array_from_shmem(winds_prev *windtable, void *memseg) {
+  int *intarray, nb_prevs, i;
+  long used_bytes;
+  winds *windarray;
+  time_t *tarray;
+
+  intarray = (int *) memseg;
+  nb_prevs = *intarray;
+  used_bytes = sizeof(int);
+  /* we are using a 4 bytes alignment, ensure that it's right (it should be) */
+  if (used_bytes % 4) {
+    used_bytes = (((used_bytes >> 2) + 1) << 2);
+  }
+
+  tarray = (time_t *) (((char *)memseg) + used_bytes);
+  windtable->time_offset = *tarray; /* might be changed to interpolation date */
+  used_bytes += sizeof(time_t);
+
+  windarray = (winds *) (((char *)memseg) + used_bytes);
+  /* free the internal structure completely */
+  if (windtable->wind != NULL) {
+    for (i=0; i<windtable->nb_prevs; i++) {
+      free(windtable->wind[i]);
+    }
+    free(windtable->wind);
+    windtable->wind = NULL;
+  }
+  windtable->wind = calloc(nb_prevs, sizeof(winds *));
+  for (i=0; i<nb_prevs; i++) {
+    windtable->wind[i] = calloc(1, sizeof(winds));
+    memcpy(windtable->wind[i], windarray++, sizeof(winds));
+  }
+  windtable->nb_prevs = nb_prevs;
+}
 
   
 
