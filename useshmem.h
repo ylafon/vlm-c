eb7991428d672c6e6b2378833826270b2d8b7c33
Yves Lafon <ylafon@w3.org>
2008-08-03 20:14:19 +0000

    modified functions

--- useshmem.h
+++ useshmem.h
@@ -1,5 +1,5 @@
 /**
- * $Id: useshmem.h,v 1.1 2008/08/03 19:53:53 ylafon Exp $
+ * $Id: useshmem.h,v 1.2 2008/08/03 20:14:19 ylafon Exp $
  *
  * (c) 2008 by Yves Lafon
  *      See COPYING file for copying and redistribution conditions.
@@ -31,7 +31,7 @@
 void shm_safe_get_wind_info_lat_long PARAM4(double, double, time_t, 
 					    wind_info *);
 
-void shm_lock_sem_construct_grib PARAM2(void **, int *);
-void shm_unlock_sem_destroy_grib PARAM2(void *, int);
+void shm_lock_sem_construct_grib PARAM1(int);
+void shm_unlock_sem_destroy_grib PARAM1(int);
 
 #endif /* _USESHMEM_H_ */
